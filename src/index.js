abc18bd907aeaa544f7c3bb5fcbade965a23e4c8
CameronRuch <cameron.ruch@yahoo.com>
2022-02-08 20:08:03 -0600

    added load, copy, click, and dblclick events

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import './less/index.less'
 
 // Your code goes here!
+
+
+// load
+window.onload = function (evt) {
+    const heading = document.querySelector('h1');
+    heading.textContent = 'All Aboard the Fun Bus!';
+
+// copy
+    window.addEventListener('copy', () => {
+        navigator.clipboard.readText()
+            .then(text => {
+                heading.textContent += text;
+            })
+    })
+
+// click
+
+document.body.addEventListener('click', evt => {
+    evt.target.classList.toggle('dup');
+
+})
+
+// dblclick
+
+document.body.addEventListener('dblclick', evt => {
+    evt.target.outerHTML = [];
+
+})
+
+
+
+}
